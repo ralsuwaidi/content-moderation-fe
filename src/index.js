d677a69e8b0b35922c440a1bae1b7fdb0d85cfec
rashed <x.suwaidi@gmail.com>
2022-12-14 13:33:49 +0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './Pages/MainPage';
+import reportWebVitals from './reportWebVitals';
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from './Pages/Common/ErrorPage';
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  }
+]);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
