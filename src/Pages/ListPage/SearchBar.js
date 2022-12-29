fee63cd1658cf8da106571e06959a6f37623e045
rashed <x.suwaidi@gmail.com>
2022-12-29 11:03:39 +0400

    add title

--- src/Pages/ListPage/SearchBar.js
+++ src/Pages/ListPage/SearchBar.js
@@ -12,19 +12,24 @@
   }
   ```
 */
+
+import { CiSearch } from "react-icons/ci";
+
+
+
 export default function SearchBar() {
   return (
     <div>
       <div className="relative mt-1 rounded-md shadow-sm">
         <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          {/* <span className="text-gray-500 sm:text-sm">$</span> */}
+          <span className="text-gray-500 "><CiSearch /></span>
         </div>
         <input
           type="text"
           name="Movie"
           id="Movie"
-          className="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-          placeholder="Avatar, Lightyear, ..."
+          className="block w-full rounded-lg border-none pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm bg-gray-100"
+          placeholder=" Avatar, Lightyear, ..."
         />
         <div className="absolute inset-y-0 right-0 flex items-center">
           <label htmlFor="currency" className="sr-only">
