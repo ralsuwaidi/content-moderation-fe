0abfef21fc00e4ae597ed2f52c058feb928faeeb
rashed <x.suwaidi@gmail.com>
2023-01-03 12:39:30 +0400

    move text end

--- src/Pages/ListPage/index.js
+++ src/Pages/ListPage/index.js
@@ -25,30 +25,32 @@ function ListPage() {
                     <div className=" px-3 mx-auto lg:pt-12 lg:px-32">
 
                         <nav className='mb-10'>
-                        <Navbar
-                            fluid={true}
-                            rounded={true}
-                        >
-                            <Navbar.Brand href="https://flowbite.com/">
-                            <img
-      src={logo}
-      className="mr-3 h-6 sm:h-9"
-      alt="Flowbite Logo"
-    />
-                                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-                                    
-                                </span>
-                            </Navbar.Brand>
-                            <Navbar.Toggle />
-                            <Navbar.Collapse>
-                                <Navbar.Link href="/navbars">
-                                    انضم إلينا
-                                </Navbar.Link>
-                            </Navbar.Collapse>
-                        </Navbar>
+                            <Navbar
+                                fluid={true}
+                                rounded={true}
+                            >
+                                <Navbar.Brand href="https://flowbite.com/">
+                                    <img
+                                        src={logo}
+                                        className="mr-3 h-6 sm:h-9"
+                                        alt="Flowbite Logo"
+                                    />
+                                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+
+                                    </span>
+                                </Navbar.Brand>
+                                <Navbar.Toggle />
+                                <Navbar.Collapse>
+                                    <Navbar.Link href="/navbars">
+                                        <div className=' text-end'>
+                                            انضم إلينا
+                                        </div>
+                                    </Navbar.Link>
+                                </Navbar.Collapse>
+                            </Navbar>
                         </nav>
 
-            
+
 
                         <p className=' text-right mb-5 text-4xl md:text-6xl font-bold'>أفلام</p>
                         <div className="pb-10">
